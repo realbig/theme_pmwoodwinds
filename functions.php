3136ae3dd21c6bd0b67673464e78517bb975d0ea
Eric Defore <eric.defore@gmail.com>
2018-12-19 07:54:59 -0500

    Closes #5

--- functions.php
+++ functions.php
@@ -819,7 +819,7 @@ function pmwoodwind_image_sizes() {
 	
 	add_image_size( 'main_image', 442, 331 );
 	add_image_size( 'product_thumbnail', 125, 94 );
-	add_image_size( 'product_grid', 252, 188 );
+	add_image_size( 'product_grid', 525, 400 );
 	add_image_size( 'home_grid', 360, 480, true );
 	
 }
