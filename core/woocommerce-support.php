56a0d5c002c32d7c869e446e77a07d17132a8d6d
Eric Defore <eric.defore@gmail.com>
2019-01-17 11:51:03 -0500

    Remove Description/Additional Information Tabs

--- core/woocommerce-support.php
+++ core/woocommerce-support.php
@@ -744,3 +744,6 @@ function pmwoodwind_product_single_primary_image_hover_zoom( $attributes, $attac
 	return $attributes;
 	
 }
+
+// Remove the Description/Additional Information Tabs
+remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10 );
