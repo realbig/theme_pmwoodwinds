c2561fb8ee1668ff81d08754a73106d214388c7c
Eric Defore <eric.defore@gmail.com>
2019-03-20 08:06:42 -0400

    Fix MSRP and Prices not changing on variation change

--- src/assets/js/lib/product-single-variations.js
+++ src/assets/js/lib/product-single-variations.js
@@ -2,9 +2,9 @@
 	
 	$( document ).on( 'found_variation', 'form', function( event, variation ) {
 		
-		$( 'p.price span.msrp' ).wc_set_content( variation.msrp.toFixed( 2 ) );
+		$( 'p.price span.msrp' ).wc_set_content( Number( variation.msrp ).toFixed( 2 ) );
 		
-		$( 'p.price span.regular-price' ).wc_set_content( variation.display_regular_price.toFixed( 2 ) );
+		$( 'p.price span.regular-price' ).wc_set_content( Number( variation.display_regular_price ).toFixed( 2 ) );
 		
 	} );
 	
